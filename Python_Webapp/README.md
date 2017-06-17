4f9e3fd934141142da20bfbf37381fce7fb1e55f
wangkai <carnegie521@sina.com>
2017-06-17 23:59:21 +0800

    one years ago learn how to build blog

---
+++ Python_Webapp/README.md
@@ -0,0 +1,4 @@
+# awesome-webapp
+# 教程地址：http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001432170937506ecfb2f6adf8e4757939732f3e32b781c000
+# 使用的版本是Python 3.5 版本
+# 代码中没有体现mysql建一个数据的过程，不过在教程中可以第三天-编写ORM中有~~
---
+++ Python_Webapp/www/config_default.py
@@ -0,0 +1,16 @@
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '',
+        #账号密码换成自己的。。。
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
+
